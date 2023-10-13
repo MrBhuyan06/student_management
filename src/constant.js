3f2061c82f7c4ec41cf312d27f99ee86d03804e8
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-10-13 23:19:03 +0530

    added the generater for creating the uuid

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,10 @@
+export const generateUniqueId = () => {
+  return Date.now().toString(36) + Math.random().toString(36).substr(2, 5);
+};
+
 export const rowData = [
   {
+    Id: generateUniqueId(),
     Name: "Abhishek",
     Dob: "06/01/2000",
     Age: 23,
@@ -9,6 +14,7 @@ export const rowData = [
     Action: "",
   },
   {
+    Id: generateUniqueId(),
     Name: "subham",
     Dob: "06/01/2000",
     Age: 23,
